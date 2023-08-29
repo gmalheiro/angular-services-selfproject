f0b137fe99d01dde2e886996831b393130cbab0e
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-28 21:02:35 -0300

    Account component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule
