0767c620955f40592e44043fa8c5cd94a6361eb6
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-29 23:24:20 -0300

    Implemented method in accounts service

--- src/app/shared/accounts.service.ts
+++ src/app/shared/accounts.service.ts
@@ -5,4 +5,9 @@ import { Injectable } from '@angular/core';
 })
 export class AccountsService {
   accounts:[{'name':string,'status':string}] = [{'name':'First element','status':'active'}];
+
+  pushToAccount(obj:any){
+    this.accounts.push(obj);
+  }
+
 }
