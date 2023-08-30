dc5f96680952149428fbee9de86049975ce89bcc
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-08-29 23:16:54 -0300

    Injected accounts service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { AccountsService } from './shared/accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[AccountsService]
 })
 export class AppComponent {
   title = 'angular-services-selfproject';
